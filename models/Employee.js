a6d734f4b23bd3b79a99e3dba85144d30df8b914
jemish kevadiya <jemishkevadiya007@gmail.com>
2025-02-15 22:36:03 -0500

    Add schemas of user and employee

---
+++ models/Employee.js
@@ -0,0 +1,51 @@
+const mongoose = require('mongoose');
+
+const EmployeeSchema = new mongoose.Schema({
+    first_name: {
+        type: String,
+        required: true
+    },
+    last_name: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true,
+        trim: true,
+        unique: true,
+        match: [
+            /^\S+@\S+\.\S+$/,
+            'Please use a valid email address',
+        ],
+    },
+    gender: {
+        type: String,
+        enum: ["Male", "Female", "other"],
+        required: true
+    },
+    designation: {
+        type: String,
+        required: true
+    },
+    salary: {
+        type: Float32Array,
+        required: true,
+        min: 1000
+    },
+    date_of_joining: {
+        type: String,
+        required: true
+    },
+    department: {
+        type: String,
+        required: true
+    },
+    employee_photo: {
+        type: String
+    }
+},{
+    timestamps: true
+})
+
+module.exports = mongoose.model('Employee', EmployeeSchema);
---
+++ models/User.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose');
+
+const UserSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    email: {
+        type: String,
+        required: true,
+        trim: true,
+        unique: true,
+        match: [
+            /^\S+@\S+\.\S+$/,
+            'Please use a valid email address',
+        ],
+    },
+    password: {
+        type: String,
+        required: true
+    }
+}, {
+    timestamps: true
+})
+
+module.exports = mongoose.model('User', UserSchema);
