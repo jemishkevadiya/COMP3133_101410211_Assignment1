73d6a56442968ceeb52d10e76340a010bc95d5d5
jemish kevadiya <jemishkevadiya007@gmail.com>
2025-02-15 22:59:03 -0500

    Add Schemas for graphQL

---
+++ schemas/schema.js
@@ -0,0 +1,43 @@
+const { gql } = require("apollo-server-express");
+
+const typeDefs = gql`
+  type User {
+    id: ID!
+    username: String!
+    email: String!
+  }
+
+  type Employee {
+    id: ID!
+    first_name: String!
+    last_name: String!
+    email: String!
+    gender: String!
+    designation: String!
+    salary: Float!
+    date_of_joining: String!
+    department: String!
+    employee_photo: String
+  }
+
+  type AuthPayload{
+    token: String
+    user: User
+  }
+
+  type Query {
+    login(email: String!, password: String!): AuthPayload
+    getAllEmployees: [Employee]
+    searchEmployeeById(id: ID!): Employee
+    searchEmployeeByDesignationOrDepartment(designation: String, department: String): [Employee]
+  }
+
+ type Mutation {
+    signup(username: String!, email: String!, password: String!): User
+    addEmployee(first_name: String!, last_name: String!, email: String!, gender: String!, designation: String!, salary: Float!, date_of_joining: String!, department: String!): Employee
+    updateEmployee(id: ID!, first_name: String, last_name: String, email: String, designation: String, salary: Float, department: String): Employee
+    deleteEmployee(id: ID!): String
+    }
+`;
+
+module.exports = typeDefs;
